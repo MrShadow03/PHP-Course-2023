a2a37799915f346a5be251bf645ce870b902acd3
Galib Jaman <gj.emon35@gmail.com>
2023-03-29 15:42:50 +0600

    inheritance, trait, static

--- OOP/oop.php
+++ OOP/oop.php
@@ -23,11 +23,45 @@ class mango extends fruit{
     }
 }
 
-$mango = new mango('Mango', 'Yellow');
+class Vehicle {
+    public $name;
+    public $color;
+    public $model;
 
-echo '<pre>';
-print_r($mango->showFruitName());
-echo '</pre>';
+    public function __construct($name, $color, $model) {
+        $this->name = $name;
+        $this->color = $color;
+        $this->model = $model;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+}
+
+trait Weapon {
+    public $fire_power = "1000";
+}
+
+class Tank extends Vehicle{
+    use Weapon;
+    public static $model_of_tank = "T-80";
+
+    public static function fire() {
+        echo "Tank is firing";
+        echo "<br>";
+    }
+}
+
+
+
+echo Tank::$model_of_tank;
+
+
+// $new_machine = new Tank("Tank", "Black", "T-80");
+// echo $new_machine->fire();
+
+
 
 
 
