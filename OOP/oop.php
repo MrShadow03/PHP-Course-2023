2d2da7bcc78068141ef25b484651dae58836e7c5
Galib Jaman <gj.emon35@gmail.com>
2023-04-01 15:59:28 +0600

    oop

--- OOP/oop.php
+++ OOP/oop.php
@@ -1,68 +1,38 @@
 <?php 
 
-class fruit{
-    public $name;
-    public $color;
+// trait Mixer{
+//     private $power = '20';
+//     public $model = "ABC";
+//     protected $price = "20k BDT";
+// }
 
-    function __construct($name, $color)
+abstract class Fruit{
+    abstract public function showName();
+    abstract public function showColor();
+}
+
+
+//Inheritance
+class Juice extends Fruit{
+    public function showName()
     {
-        $this->name = $name;
-        $this->color = $color;
+        echo "Name of the Fruit!";
     }
-
-    public function showFruitName(){
-        print "This is a ".$this->name;
-    }
-}
-
-class mango extends fruit{
-    public $species = "Fozli";
-
-    public function showSpecies(){
-        print "I am ".$this->species." Mango";
-    }
-}
-
-class Vehicle {
-    public $name;
-    public $color;
-    public $model;
-
-    public function __construct($name, $color, $model) {
-        $this->name = $name;
-        $this->color = $color;
-        $this->model = $model;
-    }
-
-    public function getName() {
-        return $this->name;
-    }
-}
-
-trait Weapon {
-    public $fire_power = "1000";
-}
-
-class Tank extends Vehicle{
-    use Weapon;
-    public static $model_of_tank = "T-80";
-
-    public static function fire() {
-        echo "Tank is firing";
-        echo "<br>";
+    public function showColor()
+    {
+        return "Name of the Fruit!";
     }
 }
 
 
 
-echo Tank::$model_of_tank;
+// Instantiation
+$juice = new Fruit;
+$juice->showName();
+//scope resolution operator (::)
 
-
-// $new_machine = new Tank("Tank", "Black", "T-80");
-// echo $new_machine->fire();
-
-
-
+$a = 1;
+$a = 2;
 
 
 
