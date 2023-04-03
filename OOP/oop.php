8c3456c21969543ba4c36eb360e082379f66afcb
Galib Jaman <gj.emon35@gmail.com>
2023-04-03 14:19:51 +0600

    final

--- OOP/oop.php
+++ OOP/oop.php
@@ -1,16 +1,15 @@
 <?php 
 
-// trait Mixer{
-//     private $power = '20';
-//     public $model = "ABC";
-//     protected $price = "20k BDT";
-// }
+trait Mixer{
+    private $power = '20';
+    public $model = "ABC";
+    protected $price = "20k BDT";
+}
 
 abstract class Fruit{
     abstract public function showName();
     abstract public function showColor();
 }
-
 
 //Inheritance
 class Juice extends Fruit{
@@ -24,15 +23,59 @@ class Juice extends Fruit{
     }
 }
 
+interface SoundInterface{
+    public static function sound($var);
+}
+
+class Car implements SoundInterface{
+    public function start(){
+        echo "Car is started!";
+    }
+    public static function sound($var){
+        echo gettype($var);
+        echo '<br>';
+    }
+}
+
+class Animal implements SoundInterface{
+    public static function sound($var){
+        echo gettype($var);
+        echo '<br>';
+    }
+}
+
+Car::sound(12);
+Animal::sound('12');
 
 
-// Instantiation
-$juice = new Fruit;
-$juice->showName();
-//scope resolution operator (::)
 
-$a = 1;
-$a = 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
